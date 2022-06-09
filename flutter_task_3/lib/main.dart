6e641b6d9a5438ea7af5ebd7327814cf56167158
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-06-09 23:55:22 +0500

    task 3

---
+++ flutter_task_3/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void  main() {
+  runApp(MyApp());
+}
+
+// Here we ar creating Statefull widget in which we can change the properties
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String fb="Facebook";
+  Color containerColor=Colors.amber;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title:Text(fb),leading:
+         Icon(
+          Icons.facebook,
+            color: Colors.white,//using icon widget
+                ),
+          ),
+          body: Container(
+            color: containerColor,
+          child: Column(
+              children: [
+                 Row(children: [
+Padding(
+                 padding: EdgeInsets.symmetric(vertical: 30,horizontal: 30),
+            child:Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.blue,
+            child: TextButton(
+                      onPressed:(){
+              setState(() {
+                        containerColor=Colors.blue;
+                  }
+     );
+   } ,
+        child: Text("Blue", style: TextStyle(color: Colors.white),),
+          ),
+      )
+      ),
+Padding(
+          padding: EdgeInsets.symmetric(vertical: 30,horizontal: 30),
+        child:Container(
+          height: 50,
+          width: 50,
+          color: Colors.red,
+          child: TextButton(
+            onPressed:(){
+              setState(() {
+                containerColor=Colors.red;
+        }
+);
+} ,
+        child: Text("RED", style: TextStyle(color: Colors.white),),
+          ),
+      )
+      )
+  ],) 
+            ],)
+          
+          ),
+  )
+  );
+
+//   }
+  }
+}
