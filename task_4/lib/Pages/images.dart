97cf9f65fa36c433b1e12ba9606bee9304fad9bd
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-06-15 16:59:30 +0500

    adding actions

--- task_4/lib/Pages/images.dart
+++ task_4/lib/Pages/images.dart
@@ -11,69 +11,126 @@ class Imagedemo extends StatefulWidget {
 }
 
 class _ImagedemoState extends State<Imagedemo> {
-  List<Container> imagelist=[] ;
-  List containerimage=['assets/Images/img_1.png','assets/Images/img_2.png','assets/Images/img_3.png','assets/Images/img_4.png','assets/Images/img_5.png','assets/Images/img_6.png'];
-int count=0;
+  //1st way
+  //List<Container> imagelist=[] ;
+ // List containerimage=['assets/Images/img_1.png','assets/Images/img_2.png','assets/Images/img_3.png','assets/Images/img_4.png','assets/Images/img_5.png','assets/Images/img_6.png'];
+// int count=0;
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return Scaffold(
+// appBar: AppBar(
+// title: Row(children: [
+//   Icon(Icons.image),
+//  Text("Images") ,
+//  ElevatedButton(
+//   onPressed:()
+//   {setState(() {
+  
+//   imagelist.add(
+//     Container(
+//       width: 90.5,
+//       height: 80.6,
+//       child: Image(
+//         image: AssetImage(
+//           containerimage[count]),
+//           width: 80,height: 70,),
+
+//   ))
+//   ;
+// if(count<containerimage.length-1){
+//    count++;
+// }
+//   else {
+//     count=0;
+//   }
+// //    counter++;
+// });
+// }, 
+//  child: Icon(Icons.add)
+//  )
+ 
+ 
+// ],),
+
+// ),
+// body:
+//  Container(
+// child:SingleChildScrollView(
+// child: Column(
+//   children:
+//     imagelist,
+// ),
+// ),
+//     )
+//     );
+// }}
+
+
+// // 2nd way of getting different images effecient way
+List<Image> imagelist=[] ;
+int count=1;
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
 appBar: AppBar(
-title: Row(children: [
-  Icon(Icons.image),
- Text("Images") ,
- ElevatedButton(
-  onPressed:()
-  {setState(() {
-  
-  imagelist.add(
-    Container(
-      width: 90.5,
-      height: 80.6,
-      child: Image(
-        image: AssetImage(
-          containerimage[count]),
-          width: 80,height: 70,),
-
-  ))
-  ;
-if(count<containerimage.length-1){
-   count++;
-}
-  else {
-    count=0;
-  }
-//    counter++;
-});
-}, 
- child: Icon(Icons.add)
- )
- 
- 
-],),
-
+  leading: IconButton(icon: Icon(Icons.menu) ,
+  onPressed:(){}),
+title: Text('MyNewApp'),
+ actions:[
+  IconButton(onPressed: (){}, icon:Icon(Icons.search) 
+  ),
+IconButton(onPressed: (){}, icon:Icon(Icons.more_vert),
+)
+],
 ),
+// Row(children: [
+//   Icon(Icons.image),
+//  Text("Images") ,
+//  ElevatedButton(
+//   onPressed:()
+//   {setState(() {
+  
+//   imagelist.add(
+//     //  Image(
+//     //     // image: AssetImage(
+//     //     //  'assets/Images/img_$count.png'),
+        
+//     //      ),
+// Image.asset('assets/Images/img_$count.png')
+  
+//   );
+
+//     count< 6 ? count++ : count=1;
+// });
+// }, 
+//  child: Icon(Icons.add)
+//  )
+ 
+ 
+// ],),
+
+// ),
 body:
  Container(
+  
 child:SingleChildScrollView(
 child: Column(
   children:
-   //[
-    // Container(color: Colors.amberAccent,
-    // height: 60,
-    // width: 60,
-    
-    // )
- // ],
     imagelist,
 ),
 ),
-    )
-    );
+    ),
+    floatingActionButton: FloatingActionButton(onPressed:() { 
+      setState(() {
+        imagelist.add(Image.asset('assets/Images/img_$count.png'));
+         count< 6 ? count++ : count=1;
+      });
 
-    // return Container(
-    //   child: Image(
-    //     image:AssetImage('assets/Images/img_2.png')
-    //     )
-    //     );
+    },
+    child:Icon(Icons.add)),
+    );
+    
+
 }}
