99166a45ead3c40c554900715ea4d8f3b79ae5a4
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2022-06-15 11:48:53 +0500

    creating list of images

--- task_4/lib/Pages/images.dart
+++ task_4/lib/Pages/images.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class Imagedemo extends StatefulWidget {
   const Imagedemo({Key? key}) : super(key: key);
@@ -10,11 +11,69 @@ class Imagedemo extends StatefulWidget {
 }
 
 class _ImagedemoState extends State<Imagedemo> {
+  List<Container> imagelist=[] ;
+  List containerimage=['assets/Images/img_1.png','assets/Images/img_2.png','assets/Images/img_3.png','assets/Images/img_4.png','assets/Images/img_5.png','assets/Images/img_6.png'];
+int count=0;
   @override
   Widget build(BuildContext context) {
-    return Container(
+
+    return Scaffold(
+appBar: AppBar(
+title: Row(children: [
+  Icon(Icons.image),
+ Text("Images") ,
+ ElevatedButton(
+  onPressed:()
+  {setState(() {
+  
+  imagelist.add(
+    Container(
+      width: 90.5,
+      height: 80.6,
       child: Image(
-        image:AssetImage('assets/Images/img_1.png')
-        )
-        );
+        image: AssetImage(
+          containerimage[count]),
+          width: 80,height: 70,),
+
+  ))
+  ;
+if(count<containerimage.length-1){
+   count++;
+}
+  else {
+    count=0;
+  }
+//    counter++;
+});
+}, 
+ child: Icon(Icons.add)
+ )
+ 
+ 
+],),
+
+),
+body:
+ Container(
+child:SingleChildScrollView(
+child: Column(
+  children:
+   //[
+    // Container(color: Colors.amberAccent,
+    // height: 60,
+    // width: 60,
+    
+    // )
+ // ],
+    imagelist,
+),
+),
+    )
+    );
+
+    // return Container(
+    //   child: Image(
+    //     image:AssetImage('assets/Images/img_2.png')
+    //     )
+    //     );
 }}
