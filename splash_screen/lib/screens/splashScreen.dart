aa037b34b5c0e0d8dfe726433aec6d1f3b11102b
ArtiDoltani <74849722+ArtiDoltani@users.noreply.github.com>
2023-03-19 23:06:18 +0500

    splash screen

---
+++ splash_screen/lib/screens/splashScreen.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:splash_screen/screens/homeScreen.dart';
+import 'package:splash_screen/screens/loginScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState(){
+    super.initState();
+   islogin();
+   
+  }
+  void islogin()async{
+    SharedPreferences sp=await SharedPreferences.getInstance();
+    bool islogin= sp.getBool('islogin')??false;
+    if(islogin){
+ Timer(
+      Duration(seconds: 10), (){
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>const Homescreen(),));
+    });
+    }
+    else{
+       Timer(
+      Duration(seconds: 10), (){
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>const LoginScreen(),));
+    });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+body: 
+ Image(
+  height:double.infinity,
+  fit: BoxFit.fitHeight,
+  image: NetworkImage('https://images.pexels.com/photos/3559235/pexels-photo-3559235.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')),
+    );
+  }
+}
